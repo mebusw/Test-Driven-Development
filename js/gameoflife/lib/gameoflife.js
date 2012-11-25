cab51b5ff546a294f7eca9dd735d6da4b37e990e
mebusw <mebusw@163.com>
2012-11-25 17:19:32 +0800

    refactoring methods for 3 cells

--- js/gameoflife/lib/gameoflife.js
+++ js/gameoflife/lib/gameoflife.js
@@ -6,24 +6,42 @@
 
 var Game = function() {
     return {
-        tick : function(init_grid) {
-            var len = init_grid[0].length;
+        tick : function(grid) {
+            this.grid = grid;
+            var len = grid[0].length;
             var result = this._initGrid(0, len);
-            for ( var i = 1; i < len - 1; i++) {
-                if (init_grid[0][i] === 1 && init_grid[0][i - 1] === 1 && init_grid[0][i + 1] === 1) {
-                    result[0][i] = 1;
-                }
-
+            // for ( var i = 1; i < len - 1; i++) {
+            // if (init_grid[0][i] === 1 && init_grid[0][i - 1] === 1 &&
+            // init_grid[0][i + 1]
+            // === 1) {
+            // result[0][i] = 1;
+            // }
+            //
+            // }
+            if (this._isLeftAlive() && this._isAlive() && this._isRightAlive()) {
+                result[0][1] = 1;
             }
             return result;
         },
 
         _initGrid : function(elem, len) {
-            var arr = [[]];
+            var arr = [ [] ];
             for ( var i = 0; i < len; i++) {
                 arr[0].push(elem);
             }
             return arr;
+        },
+
+        _isAlive : function() {
+            return this.grid[0][1] === 1;
+        },
+
+        _isLeftAlive : function() {
+            return this.grid[0][0] === 1;
+        },
+
+        _isRightAlive : function() {
+            return this.grid[0][2] === 1;
         },
     };
 

0dc1a1c17312e04412cab8fb39221fc6c524b026
mebusw <mebusw@163.com>
2012-12-04 22:43:28 +0800

    find pair

--- python/src/pokerHands/pokerHands.py
+++ python/src/pokerHands/pokerHands.py
@@ -9,14 +9,34 @@ class Game(object):
     classdocs
     '''
 
-
     def __init__(self):
         '''
         Constructor
         '''
-        pass
+        self.value = list('23456789TJQKA')
     
     def category(self, hand):
-        self.highest = 'K'
+        self._parseHand(hand)
+        self._findHighestCard()
+        self._countCards()
+
+        for v in self.counter.itervalues():
+            if v > 1:
+                return 'pair'
         return 'high card'
+    
+    def _parseHand(self, hand):
+        self.cards = hand.split(' ')   
+            
+    def _findHighestCard(self):
+        self.highest = reduce(lambda x, y: 
+                              x if self.value.index(x[0]) >= self.value.index(y[0]) else y, self.cards)
+        
+    def _countCards(self):
+        self.counter = {}
+        for c in self.cards:
+            if self.counter.has_key(c):
+                self.counter[c] += 1
+            else:
+                self.counter[c] = 1
         
--- python/src/pokerHands/pokerHandsTest.py
+++ python/src/pokerHands/pokerHandsTest.py
@@ -23,11 +23,17 @@ class Test(unittest.TestCase):
     def testHighCard(self):
         hand = '2H 3D 5S 9C KD'
         self.assertEqual('high card', self.game.category(hand))
-        self.assertEqual('K', self.game.highest)
+        self.assertEqual('KD', self.game.highest)
         
         hand = '2C 3H 4S 8C AH'
         self.assertEqual('high card', self.game.category(hand))
-        self.assertEqual('A', self.game.highest)
+        self.assertEqual(['2C', '3H', '4S', '8C', 'AH'], self.game.cards)
+        self.assertEqual('AH', self.game.highest)
+
+    def testPair(self):
+        hand = '2H 2H 5S 9C KD'
+        self.assertEqual('pair', self.game.category(hand))
+        self.assertEqual({'2H':2, '5S':1, '9C':1, 'KD':1}, self.game.counter)
 
 
 if __name__ == "__main__":

898dc1723023af4abf613b0603ba2088f12726ea
mebusw <mebusw@163.com>
2013-02-21 14:03:32 +0800

    Then extract duplication by: -Extract Method (Alt-Shift-M) -Rename (Alt-Shift-R)

--- java/src/training/budgetPeriod/BudgetCategory.java
+++ java/src/training/budgetPeriod/BudgetCategory.java
@@ -11,13 +11,7 @@ public class BudgetCategory {
         // If Start and End are in the same budget period
         if (getBudgetPeriodType().getStartOfBudgetPeriod(startDate).equals(
                 getBudgetPeriodType().getStartOfBudgetPeriod(endDate))) {
-            long amount = getAmount(startDate);
-            long daysInPeriod = getBudgetPeriodType()
-                    .getDaysInPeriod(startDate);
-            long daysBetween = DateUtil
-                    .getDaysBetween(startDate, endDate, true);
-            double amountInPeriod = ((double) amount / (double) daysInPeriod)
-                    * daysBetween;
+            double amountInPeriod = getAmountInPeriod(startDate, endDate);
             return (long) amountInPeriod;
         }
         // If the area between Start and End overlap at least two budget
@@ -27,15 +21,10 @@ public class BudgetCategory {
                 || getBudgetPeriodType().getBudgetPeriodOffset(startDate, 1)
                         .before(getBudgetPeriodType().getStartOfBudgetPeriod(
                                 endDate))) {
-            long amountStartPeriod = getAmount(startDate);
-            long daysInStartPeriod = getBudgetPeriodType().getDaysInPeriod(
-                    startDate);
             Date endOfBudgetPeriod = getBudgetPeriodType()
                     .getEndOfBudgetPeriod(startDate);
-            long daysAfterStartDateInStartPeriod = DateUtil.getDaysBetween(
-                    startDate, endOfBudgetPeriod, true);
-            double totalStartPeriod = ((double) amountStartPeriod / (double) daysInStartPeriod)
-                    * daysAfterStartDateInStartPeriod;
+            double totalStartPeriod = getAmountInPeriod(startDate,
+                    endOfBudgetPeriod);
 
             double totalInMiddle = 0;
             for (String periodKey : getBudgetPeriods(getBudgetPeriodType()
@@ -46,13 +35,8 @@ public class BudgetCategory {
 
             Date startOfBudgetPeriod = getBudgetPeriodType()
                     .getStartOfBudgetPeriod(endDate);
-            long amountEndPeriod = getAmount(startOfBudgetPeriod);
-            long daysInEndPeriod = getBudgetPeriodType().getDaysInPeriod(
-                    startOfBudgetPeriod);
-            long daysBeforeEndDateInEndPeriod = DateUtil.getDaysBetween(
-                    startOfBudgetPeriod, endDate, true);
-            double totalEndPeriod = ((double) amountEndPeriod / (double) daysInEndPeriod)
-                    * daysBeforeEndDateInEndPeriod;
+            double totalEndPeriod = getAmountInPeriod(startOfBudgetPeriod,
+                    endDate);
 
             return (long) (totalStartPeriod + totalInMiddle + totalEndPeriod);
         }
@@ -60,6 +44,18 @@ public class BudgetCategory {
                 "You should not be here. We have returned all legitimate numbers from getAmount(Date, Date) in BudgetCategoryImpl.Please contact Wyatt Olson with details on how you got here (what steps did you perform in Buddi to get this error message).");
     }
 
+    private double getAmountInPeriod(Date startDate, Date endDate) {
+        long amountOfPeriod = getAmount(startDate);
+        long totalDaysInPeriod = getBudgetPeriodType().getDaysInPeriod(
+                startDate);
+        long daysInPeriod = DateUtil.getDaysBetween(
+                startDate, endDate, true);
+        double totalInPeriod = ((double) amountOfPeriod / (double) totalDaysInPeriod)
+                * daysInPeriod;
+        return totalInPeriod;
+    }
+
+    ////////// Below are stubs
     private Date getPeriodDate(String periodKey) {
         // TODO Auto-generated method stub
         return new Date();

9a007272c6ef4db97f41788a39efe8f194c31157
mebusw <mebusw@163.com>
2013-02-24 01:14:58 +0800

    add tests for ArgsException

--- java/src/training/args/ArgsException.java
+++ java/src/training/args/ArgsException.java
@@ -53,15 +53,15 @@ public class ArgsException extends Exception {
         case UNEXPECTED_ARGUMENT:
             return String.format("Argument -%c unexpected.", errorArgumentId);
         case MISSING_DOUBLE:
-            return String.format("Could not find double parameter for -%c", errorArgumentId);
+            return String.format("Could not find double parameter for -%c.", errorArgumentId);
         case INVALID_DOUBLE:
             return String.format("Argument -%c expects a double but was '%s'.", errorArgumentId, errorParameter);
         case MISSING_INTEGER:
-            return String.format("Could not find integer parameter for -%c", errorArgumentId);
+            return String.format("Could not find integer parameter for -%c.", errorArgumentId);
         case INVALID_INTEGER:
             return String.format("Argument -%c expects an integer but was '%s'.", errorArgumentId, errorParameter);
         case MISSING_STRING:
-            return String.format("Could not find string parameter for -%c", errorArgumentId);
+            return String.format("Could not find string parameter for -%c.", errorArgumentId);
         case OK:
             throw new Exception("TILT: should not get here.");
         }
---
+++ java/tst/training/args/ArgsExceptionTest.java
@@ -0,0 +1,44 @@
+package training.args;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ArgsExceptionTest {
+
+    @Test
+    public void testUnexpectedMessage() throws Exception {
+        ArgsException e = new ArgsException(ArgsException.ErrorCode.UNEXPECTED_ARGUMENT, 'x', null);
+        assertEquals("Argument -x unexpected.", e.errorMessage());
+    }
+
+    @Test
+    public void testMissingStringMessage() throws Exception {
+        ArgsException e = new ArgsException(ArgsException.ErrorCode.MISSING_STRING, 'x', null);
+        assertEquals("Could not find string parameter for -x.", e.errorMessage());
+    }
+
+    @Test
+    public void testinvalidIntegerMessage() throws Exception {
+        ArgsException e = new ArgsException(ArgsException.ErrorCode.INVALID_INTEGER, 'x', "Forty Two");
+        assertEquals("Argument -x expects an integer but was 'Forty Two'.", e.errorMessage());
+    }
+
+    @Test
+    public void testMissingIntegerMessage() throws Exception {
+        ArgsException e = new ArgsException(ArgsException.ErrorCode.MISSING_INTEGER, 'x', null);
+        assertEquals("Could not find integer parameter for -x.", e.errorMessage());
+    }
+    
+    @Test
+    public void testinvalidDoubleMessage() throws Exception {
+        ArgsException e = new ArgsException(ArgsException.ErrorCode.INVALID_DOUBLE, 'x', "Forty Two");
+        assertEquals("Argument -x expects a double but was 'Forty Two'.", e.errorMessage());
+    }
+
+    @Test
+    public void testMissingDoubleMessage() throws Exception {
+        ArgsException e = new ArgsException(ArgsException.ErrorCode.MISSING_DOUBLE, 'x', null);
+        assertEquals("Could not find double parameter for -x.", e.errorMessage());
+    }
+}

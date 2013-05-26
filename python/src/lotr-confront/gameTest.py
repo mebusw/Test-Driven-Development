dd6606807214c8f0e567dcdbdaccb650eb4a1179
mebusw <mebusw@163.com>
2013-05-26 23:36:58 +0800

    pair work with Terry: Test drive out while spawns meet up and resolve only by their strengh

--- python/src/lotr-confront/gameTest.py
+++ python/src/lotr-confront/gameTest.py
@@ -1,7 +1,7 @@
 '''
 Created on May 20, 2013
 
-@author: 
+@author: Jacky, Terry
 '''
 import unittest
 from game import *
@@ -10,48 +10,43 @@ class Test(unittest.TestCase):
 
 
     def setUp(self):
-        self.game = Game()
-
+        pass
 
     def tearDown(self):
         pass
 
+    def test_player1_move_spawn(self):
+        game = Game()
+        spawn = Spawn('Frodo',1 , 'Expedit') 
+        game.setSpawn(spawn, "Hollin")
+        game.player1Move(spawn, dest = "Moria")
 
-    def testResolveOnlyByStrengh_player1Win(self):
-        self.game.player1_send_spawn(Gandalf())
-        self.game.player2_send_spawn(Golum())        
-        
-        self.game.resolve()
-        
-        self.assertTrue(self.game.player1.alive)
-        self.assertFalse(self.game.player2.alive)
+        self.assertIn(spawn, game.spawnsAt("Moria"))
+        self.assertNotIn(spawn, game.spawnsAt("Hollin"))
 
-    def testResolveOnlyByStrengh_player2Win(self):
-        self.game.player1_send_spawn(Gandalf())
-        self.game.player2_send_spawn(Aragon())        
-        
-        self.game.resolve()
-        
-        self.assertFalse(self.game.player1.alive)
-        self.assertTrue(self.game.player2.alive)
 
-    def testResolveOnlyByStrengh_draw(self):
-        self.game.player1_send_spawn(Gandalf())
-        self.game.player2_send_spawn(Saruman())        
-        
-        self.game.resolve()
-        
-        self.assertFalse(self.game.player2.alive)
-        self.assertFalse(self.game.player1.alive)
+    def test_player2_meet_player1_then_stronger_win(self):
+        game = Game()
+        spawn1 = Spawn('Sam', 2, 'Expedit')
+        spawn2 = Spawn('Shelob', 5, 'Sauron')
+        game.setSpawn(spawn1, "Moria")
+        game.setSpawn(spawn2, "Moria")
+        game.resolve("Moria")
 
-    def testResolveByAblity_Lagolas_beat_Dragon(self):
-        self.game.player1_send_spawn(Lagolas())
-        self.game.player2_send_spawn(Dragon())        
-        
-        self.game.resolve()
-        
-        self.assertTrue(self.game.player1.alive)
-        self.assertFalse(self.game.player2.alive)
+        self.assertIn(spawn2, game.spawnsAt("Moria"))
+        self.assertNotIn(spawn1, game.spawnsAt("Moria"))
+
+    def test_player2_meet_player1_then_both_die(self):
+        game = Game()
+        spawn1 = Spawn('Sam', 2, 'Expedit')
+        spawn2 = Spawn('Warg', 2, 'Sauron')
+        game.setSpawn(spawn1, "Moria")
+        game.setSpawn(spawn2, "Moria")
+        game.resolve("Moria")
+
+        self.assertNotIn(spawn2, game.spawnsAt("Moria"))
+        self.assertNotIn(spawn1, game.spawnsAt("Moria"))
+
         
 
 if __name__ == "__main__":

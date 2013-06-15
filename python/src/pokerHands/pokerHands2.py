21f8a7d9205fb9ef4cef76308d6c2eccbd0a3123
mebusw <mebusw@163.com>
2013-06-15 18:41:55 +0800

    re implement pokerHand

--- python/src/pokerHands/pokerHands2.py
+++ python/src/pokerHands/pokerHands2.py
@@ -6,28 +6,53 @@ Created on 2013-6-15
 
 RANKS = {'2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, 'T':10, 'J':11, 'Q':12, 'K':13, 'A':14}
 
-SUITE_RANKS = {'D':1, 'C':2, 'H':3, 'S':4} 
-
-class Game(object):
-    '''
-    classdocs
-    '''
-    def __init__(self):
-        '''
-        Constructor
-        '''
-
-
 class Hand(object):
-	def __init__(self, cardString):
-		self.cards = cardString.split(' ')
-		self.rankedCards = map(lambda card: RANKS[card[0]], self.cards)
-		self.rankedCards.sort(reverse=True)
-		
-		self._features = [self._isFlush(), self.rankedCards]
+    def __init__(self, cardString):
+        self.cards = cardString.split(' ')
+        self.rankedCards = map(lambda card: RANKS[card[0]], self.cards)
+        self.rankedCards.sort(reverse=True)
+        
+        self.pairs = []
+        self.fourKinds = []
+        self.threeKinds = []
+        self._findDuplicates()
 
-	def __cmp__(self, other):
-		return cmp(self._features, other._features)
+        self._features = [
+            self._isStraight() and self._isFlush(), 
+            self.fourKinds, 
+            self._isFullHouse(), 
+            self._isFlush(), 
+            self._isStraight(), 
+            self.threeKinds, 
+            len(self.pairs), 
+            self.pairs, 
+            self.rankedCards]
 
-	def _isFlush(self):
-		return all(self.cards[0][1] == c[1] for c in self.cards)
+    def __cmp__(self, other):
+        return cmp(self._features, other._features)
+
+    def _isFullHouse(self):
+        return len(self.threeKinds) == 1 and len(self.pairs) == 1
+
+    def _isFlush(self):
+        return all(self.cards[0][1] == c[1] for c in self.cards)
+
+    def _isStraight(self):
+        return all([z[0] == z[1] + 1 for z in zip(self.rankedCards[:-1], self.rankedCards[1:])])
+
+    def _findDuplicates(self):
+        self._duplicates = {}
+        for c in self.cards:
+            if self._duplicates.has_key(c[0]):
+                self._duplicates[c[0]] += 1
+            else:
+                self._duplicates[c[0]] = 1
+
+        for k, v in self._duplicates.iteritems():
+            if v == 4:
+                self.fourKinds.append(k)
+            if v == 3:
+                self.threeKinds.append(k)
+            if v == 2:
+                self.pairs.append(k)
+        self.pairs.sort(reverse=True)
--- python/src/pokerHands/pokerHands2Test.py
+++ python/src/pokerHands/pokerHands2Test.py
@@ -4,27 +4,50 @@ Created on 2013-6-15
 @author: mebusw@gmail.com
 '''
 import unittest
-from pokerHands2 import Game, Hand
+from pokerHands2 import Hand
 
 
-class GameTest(unittest.TestCase):
-
-
+class HandTest(unittest.TestCase):
     def setUp(self):
-        self.game = Game()
-
+        pass
 
     def tearDown(self):
         pass
 
-
-    def testHighCards(self):
+    def testHighCards_HigherShouldWin(self):
         self.assertGreater(Hand('2C 3H 4S 8C AH'), Hand('2H 3D 5S 9C KD'))
 
-    def testFlushBeatHighCards(self):
-        self.assertGreater(Hand('9S 3S 4S KS 6S'), Hand('2C 3H 4S 8C AH'))
+    def testHighCards_ShouldIgnoreSuites(self):
+        self.assertEqual(Hand('2C 3H 4S 8C AH'), Hand('2H 3D 4S 8C AD'))
 
+    def testOnePairBearHighCard(self):
+        self.assertGreater(Hand('2H 3D 5S KS KD'), Hand('2C 3H 4S 8C AH'))    
         
+    def testTwoPairsBeatOnePair(self): 
+        self.assertGreater(Hand('2C 4H 4S 8C 8H'), Hand('2H 3D 5S 5S 3D'))  
+
+    def testThreeKindBeatTwoPairs(self): 
+        self.assertGreater(Hand('2H 5S 5S 5S KD'), Hand('2H 3D 5S 5S 3D'))  
+
+    def testStraightBeatThreeKind(self):
+        self.assertGreater(Hand('9S TS JC QS KD'), Hand('2C 3H 4S 5C 6H'))        
+
+    def testFlushBeatStraight(self):
+        self.assertGreater(Hand('9S 3S JS QS KS'), Hand('9S TS JC QS KD'))  
+
+    def testFullHouseBeatFlush(self):
+        self.assertGreater(Hand('5C 3H 3S 3C 5H'), Hand('5S 9S 7S 2S 3S'))
+
+    def testBiggerFullHouseBeatSmallerFullHouse(self):
+        self.assertGreater(Hand('5C 6H 6S 6C 5H'), Hand('4S 4D 4S 6S 6S'))
+
+    def testFourKindsBeatFullHouse(self):
+        self.assertGreater(Hand('9S 9S 9S 9S 6S'), Hand('2C 3H 3S 3C 2H'))
+
+    def testStraightFlushBeatFourKind(self):
+        self.assertGreater(Hand('5C 6C 7C 8C 9C'), Hand('9S 9S 9S 9S 6S'))
+
+
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
     unittest.main()

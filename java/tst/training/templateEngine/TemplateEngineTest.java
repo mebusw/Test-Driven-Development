9c48ca8fc60c8032fd61d692d6d66aa23eb5393a
mebusw <mebusw@163.com>
2013-09-08 12:41:25 +0800

    complete evaluate2() and to be refactoring to patterns

--- java/tst/training/templateEngine/TemplateEngineTest.java
+++ java/tst/training/templateEngine/TemplateEngineTest.java
@@ -22,24 +22,24 @@ public class TemplateEngineTest {
 
     @Test
     public void testTemplateWithNoVariable() {
-        engine.compile("Hello");
+        engine.setTemplateString("Hello");
 
         assertEquals("Hello", engine.evaluate(placeHolders));
     }
 
     @Test
     public void testTemplateWithOneVariable() {
-        engine.compile("Hello, $name");
-        placeHolders.put("$name", "Jacky");
+        engine.setTemplateString("Hello, ${name}");
+        placeHolders.put("${name}", "Jacky");
 
         assertEquals("Hello, Jacky", engine.evaluate(placeHolders));
     }
 
     @Test
     public void testTemplateWithTwoVariables() {
-        engine.compile("Hello, $name $family");
-        placeHolders.put("$name", "Jacky");
-        placeHolders.put("$family", "Shen");
+        engine.setTemplateString("Hello, ${name} ${family}");
+        placeHolders.put("${name}", "Jacky");
+        placeHolders.put("${family}", "Shen");
 
         assertEquals("Hello, Jacky Shen", engine.evaluate(placeHolders));
     }
@@ -56,12 +56,17 @@ public class TemplateEngineTest {
     public void testCompileTemplateOfPureTextToSegments() {
         List<String> segments = engine.compile("Hello");
         assertSegments(segments, "Hello");
+
     }
 
     @Test
     public void testCompileTemplateToSegments() {
-        List<String> segments = engine.compile("Hello ${aaa} ${bbb}");
-        assertSegments(segments, "Hello ", "aaa", " ", "bbb");
+        List<String> segments = engine.compile("Hello, ${name} ${family}");
+        assertSegments(segments, "Hello, ", "${name}", " ", "${family}");
+        placeHolders.put("${name}", "Jacky");
+        placeHolders.put("${family}", "Shen");
+
+        assertEquals("Hello, Jacky Shen", engine.evaluate2(placeHolders));
     }
 
     ///////////////////
@@ -73,7 +78,12 @@ public class TemplateEngineTest {
 
 class TemplateEngine {
 
+    void setTemplateString(String templateString) {
+        this.templateString = templateString;
+    }
+
     private String templateString;
+    private List<String> segments;
 
     public String evaluate(Map<String, String> placeHolders) {
         for (Map.Entry<String, String> entry : placeHolders.entrySet()) {
@@ -83,23 +93,51 @@ class TemplateEngine {
     }
 
     public List<String> compile(String templateString) {
-        this.templateString = templateString;
-        List<String> segments = new ArrayList<String>();
-        Pattern pattern = Pattern.compile("\\$\\{([^}]*)\\}");
+        segments = new ArrayList<String>();
+        int index = collectSegments(templateString, segments);
+        addTailText(templateString, segments, index);
+        return segments;
+    }
+
+
+    public String evaluate2(Map<String, String> placeHolders) {
+        StringBuffer sb = new StringBuffer();
+        for (String seg : segments) {
+            if (seg.startsWith("${")) {
+                sb.append(placeHolders.get(seg));
+            } else {
+                sb.append(seg);
+            }
+        }
+        return sb.toString();
+    }
+
+    private int collectSegments(String templateString, List<String> segments) {
+        Pattern pattern = Pattern.compile("\\$\\{[^}]*\\}");
         Matcher matcher = pattern.matcher(templateString);
 
         int index = 0;
         while (matcher.find()) {
-            segments.add(templateString.substring(index, matcher.start()));
+            appendPreceedingText(templateString, segments, matcher, index);
 
-            segments.add(matcher.group(1));
+            appendVariable(segments, matcher);
             index = matcher.end();
         }
+        return index;
+    }
 
+    private void addTailText(String templateString, List<String> segments, int index) {
         if (index < templateString.length()) {
             segments.add(templateString.substring(index));
         }
-        return segments;
+    }
+
+    private void appendVariable(List<String> segments, Matcher matcher) {
+        segments.add(matcher.group(0));
+    }
+
+    private void appendPreceedingText(String templateString, List<String> segments, Matcher matcher, int index) {
+        segments.add(templateString.substring(index, matcher.start()));
     }
 
 

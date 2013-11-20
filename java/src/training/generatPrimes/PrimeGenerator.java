f4e8d405cb7848c19ef09550454543c3f149884f
mebusw <mebusw@163.com>
2013-11-20 12:11:26 +0800

    make the method as instance not static

--- java/src/training/generatPrimes/PrimeGenerator.java
+++ java/src/training/generatPrimes/PrimeGenerator.java
@@ -1,75 +1,65 @@
 package training.generatPrimes;
 
-public class PrimeGenerator {
-	private static final int TWO = 2;
-	private static boolean[] isCrossed;
+/**
+ * Refactorings:
+ * 
+ * Extract Fields.
+ * Extract Methods: initArrayOfIntegers, crossOutMultiples, putUncrossedIntegerIntoResult
+ * Inline s with f.length
+ * Rename f to isCrossed
+ * Ensure for loop starts from 2
+ * Extract Methods: crossOutMultipleOf, numberOfUncrossedIntegers, notCrossed
+ * Rename methods to: uncrossIntegersUpTo
+ * @author jacky
+ * 
+ */
 
+public class PrimeGenerator {
 	/**
 	 * 
 	 * @param maxValue
 	 *            is the generation limit
 	 * @return
 	 */
-	public static int[] generatePrimes(int maxValue) {
+	public int[] generatePrimes(int maxValue) {
 		if (maxValue >= 2) { // the only valid case
-			uncrossIntegersUpTo(maxValue);
-			crossOutMultiples();
-			return putUncrossedToResult();
+			// declarations
+			int s = maxValue + 1; // size of array
+			boolean[] f = new boolean[s];
+
+			// initialize array to true
+			for (int i = 0; i < s; i++) {
+				f[i] = true;
+			}
+
+			// get rid of known non-primes
+			f[0] = f[1] = false;
+
+			// sieve
+			for (int i = 2; i < Math.sqrt(s) + 1; i++) {
+				for (int j = 2 * i; j < s; j += i) {
+					f[j] = false; // multiple is not prime
+				}
+			}
+
+			// how many primes are there?
+			int count = 0;
+			for (int i = 0; i < s; i++) {
+				if (f[i])
+					count++; // bump count
+			}
+
+			int[] primes = new int[count];
+
+			// move the primes into the result
+			for (int i = 0, j = 0; i < s; i++) {
+				if (f[i])
+					primes[j++] = i;
+			}
+
+			return primes;
 		} else { // maxValue < 2
 			return new int[0]; // return null array if bad input.
-		}
-	}
-
-	private static int[] putUncrossedToResult() {
-		int[] result = new int[numberOfUncrossedIntegers()];
-
-		// move the primes into the result
-		for (int i = TWO, j = 0; i < isCrossed.length; i++) {
-			if (notCrossed(i))
-				result[j++] = i;
-		}
-		return result;
-	}
-
-	private static int numberOfUncrossedIntegers() {
-		// how many primes are there?
-		int count = 0;
-		for (int i = TWO; i < isCrossed.length; i++) {
-			if (notCrossed(i))
-				count++; // bump count
-		}
-		return count;
-	}
-
-	private static void crossOutMultiples() {
-		// sieve
-		for (int i = TWO; i < determineInterationLimit(); i++) {
-			if (notCrossed(i)) {
-				crossOutMultipleOf(i);
-			}
-		}
-	}
-
-	private static void crossOutMultipleOf(int i) {
-		for (int j = 2 * i; j < isCrossed.length; j += i) {
-			isCrossed[j] = true; // multiple is not prime
-		}
-	}
-
-	private static double determineInterationLimit() {
-		return Math.sqrt(isCrossed.length) + 1;
-	}
-
-	private static boolean notCrossed(int i) {
-		return !isCrossed[i];
-	}
-
-	private static void uncrossIntegersUpTo(int maxValue) {
-		isCrossed = new boolean[maxValue + 1];
-
-		// initialize array to true
-		for (int i = TWO; i < isCrossed.length; i++) {
-			isCrossed[i] = false;
 		}
 	}
 }
--- java/tst/training/generatPrimes/PrimeGeneratorTest.java
+++ java/tst/training/generatPrimes/PrimeGeneratorTest.java
@@ -8,19 +8,19 @@ public class PrimeGeneratorTest {
 
 	@Test
 	public void testPrimes() {
-		int[] nullArray = PrimeGenerator.generatePrimes(0);
+		int[] nullArray = new PrimeGenerator().generatePrimes(0);
 		assertEquals(0, nullArray.length);
 
-		int[] minArray = PrimeGenerator.generatePrimes(2);
+		int[] minArray = new PrimeGenerator().generatePrimes(2);
 		assertEquals(1, minArray.length);
 		assertEquals(2, minArray[0]);
 
-		int[] threeArray = PrimeGenerator.generatePrimes(3);
+		int[] threeArray = new PrimeGenerator().generatePrimes(3);
 		assertEquals(2, threeArray.length);
 		assertEquals(2, threeArray[0]);
 		assertEquals(3, threeArray[1]);
 
-		int[] centArray = PrimeGenerator.generatePrimes(100);
+		int[] centArray = new PrimeGenerator().generatePrimes(100);
 		assertEquals(25, centArray.length);
 		assertEquals(97, centArray[24]);
 
@@ -29,7 +29,7 @@ public class PrimeGeneratorTest {
 	@Test
 	public void testExhaustive() {
 		for (int i = 2; i < 500; i++) {
-			verifyPrimeList(PrimeGenerator.generatePrimes(i));
+			verifyPrimeList(new PrimeGenerator().generatePrimes(i));
 		}
 
 	}

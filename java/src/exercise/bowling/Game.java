be65a463c11e53a2eafb49155afed83346585aa1
mebusw <mebusw@163.com>
2013-11-28 00:56:03 +0800

    Refactoring BowlingGame

--- java/src/exercise/bowling/Game.java
+++ java/src/exercise/bowling/Game.java
@@ -2,61 +2,45 @@ package exercise.bowling;
 
 public class Game {
 
-	private int[] itsThrows = new int[21];
-	private int itsCurrentThrow = 0;
 	private boolean firstThrowInFrame = true;
 	private int itsCurrentFrame = 1;
-	private int ball;
-	private int firstThrow;
-	private int secondThrow;
+	private Scorer itsScorer = new Scorer();
 
 	public void add(int pins) {
-		itsThrows[itsCurrentThrow++] = pins;
+		itsScorer.addThrow(pins);
 		adjustCurrentFrame(pins);
 	}
 
 	private void adjustCurrentFrame(int pins) {
 		if (firstThrowInFrame == true) {
-			if (pins == 10) {
-				itsCurrentFrame++;
+			if (!adjustFrameForStrike(pins)) {
+				firstThrowInFrame = false;
 			}
-			firstThrowInFrame = false;
 		} else {
 			firstThrowInFrame = true;
-			itsCurrentFrame++;
+			advanceFrame();
 		}
-		itsCurrentFrame = Math.min(11, itsCurrentFrame);
+
 	}
 
-	public int score() {
-		return scoreForFrame(getCurrentFrame() - 1);
+	private boolean adjustFrameForStrike(int pins) {
+		if (pins == 10) {
+			advanceFrame();
+			return true;
+		}
+		return false;
+	}
+
+	private void advanceFrame() {
+		itsCurrentFrame = Math.min(11, itsCurrentFrame + 1);
 	}
 
 	public int scoreForFrame(int frame) {
-		int score = 0;
-		ball = 0;
-		for (int currrentFrame = 0; currrentFrame < frame; currrentFrame++) {
-			firstThrow = itsThrows[ball++];
-			if (firstThrow == 10) {
-				score += 10 + itsThrows[ball] + itsThrows[ball + 1];
-			} else {
-				score += handleSecondThrow();
-			}
-		}
-		return score;
+		return itsScorer.scoreForFrame(frame);
 	}
 
-	private int handleSecondThrow() {
-		int score = 0;
-		secondThrow = itsThrows[ball++];
-		int frameScore = firstThrow + secondThrow;
-		// spare need next frame's first throw
-		if (frameScore == 10) {
-			score += frameScore + itsThrows[ball];
-		} else {
-			score += frameScore;
-		}
-		return score;
+	public int score() {
+		return itsScorer.scoreForFrame(getCurrentFrame() - 1);
 	}
 
 	public int getCurrentFrame() {

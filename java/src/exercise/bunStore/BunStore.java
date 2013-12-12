7e1c1ba819876a18a0959bd4f2ddac60f448fc64
mebusw <mebusw@163.com>
2013-12-12 21:32:31 +0800

    do BunStoreKata again, step1 & step2

--- java/src/exercise/bunStore/BunStore.java
+++ java/src/exercise/bunStore/BunStore.java
@@ -2,175 +2,63 @@ package exercise.bunStore;
 
 public class BunStore {
 
-	private StuffingSource stuffingSource;
-
-	public BunStore() {
+	public Bun orderQF(String bunName) {
+		Bun bun = new QFCooker().make(bunName);
+		return bun;
 	}
 
-	public Bun orderQF(String bunType) {
-		return new Waiter().make(CookQF.make(bunType, stuffingSource));
+	public Bun orderGBL(String bunName) {
+		Bun bun = new GBLCooker().make(bunName);
+		return bun;
+
 	}
 
-	public Bun orderGBL(String bunType) {
-		return new Waiter().make(CookGBL.make(bunType, stuffingSource));
+}
+
+class Cooker {
+	protected String style;
+
+	public Bun make(String bunName) {
+		Bun bun = new Bun();
+		mix(bun, bunName);
+		kneadAndWrap(bun, bunName);
+		steamAndDishOut(bun);
+		return bun;
 	}
 
-	public void setStuffingSource(StuffingSource stuffingSource) {
-		this.stuffingSource = stuffingSource;
+	public void kneadAndWrap(Bun bun, String bunStyle) {
+		bun.content.append(String.format(
+				"Kneaded into dough in %s style. Wrapped buns in %s style. ",
+				style, style));
 	}
 
+	public void mix(Bun bun, String bunName) {
+		bun.content.append(String.format(
+				"Mixed stuffing of %s bun in %s style. ", bunName, style));
+	}
+
+	private void steamAndDishOut(Bun bun) {
+		bun.content.append("Steamed buns. Dished out buns.");
+	}
+}
+
+class QFCooker extends Cooker {
+	public QFCooker() {
+		style = "QF";
+	}
+
+}
+
+class GBLCooker extends Cooker {
+	public GBLCooker() {
+		style = "GBL";
+	}
 }
 
 class Bun {
-	public StringBuffer sb = new StringBuffer();
+	protected StringBuffer content = new StringBuffer();
 
-	public String toString() {
-		return sb.toString();
-	}
-
-}
-
-// ////////// Abstract Factory
-interface StuffingSource {
-	public Bun mix(Bun bun, String stuffing, String style);
-
-}
-
-class ManualStuffing implements StuffingSource {
-	public Bun mix(Bun bun, String stuffing, String style) {
-		bun.sb.append(String.format("Mixed stuffing of %s bun in %s style.",
-				stuffing, style));
-		return bun;
-	}
-}
-
-class FactoryStuffing implements StuffingSource {
-
-	public Bun mix(Bun bun, String stuffing, String style) {
-		bun.sb.append(String
-				.format("Got mixed stuffing of %s bun from %s Ingredient Factory. Got flour from %s Ingredient Factory. Prepared stuffing.",
-						stuffing, style, style));
-		return bun;
-
-	}
-
-}
-
-// //////////////
-class BunCooker {
-	protected Bun bun;
-	protected String style = "===";
-	protected StuffingSource stuffingSource;
-	protected String stuffing = ">>>";
-
-	public void setStuffingSource(StuffingSource stuffingSource) {
-		this.stuffingSource = stuffingSource;
-	}
-
-	public BunCooker() {
-		bun = new Bun();
-	}
-
-	public Bun kneadAndWrap() {
-		bun.sb.append(String.format(
-				" Kneaded into dough in %s style. Wrapped buns in %s style. ",
-				style, style));
-		return bun;
-	}
-
-	public Bun mix() {
-		stuffingSource.mix(bun, stuffing, style);
-		return bun;
-	}
-
-}
-
-class QFBunCooker extends BunCooker {
-	public QFBunCooker() {
-		style = "QF";
-	}
-}
-
-class GBLBunCooker extends BunCooker {
-	public GBLBunCooker() {
-		style = "GBL";
-
-	}
-}
-
-// /////////////////
-// / Factory
-
-class GOP_QFBunCooker extends QFBunCooker {
-
-	public GOP_QFBunCooker() {
-		stuffing = "Green Onion and Pork";
-	}
-}
-
-class SSP_QFBunCooker extends QFBunCooker {
-	public SSP_QFBunCooker() {
-		stuffing = "Sam Sum and Pork";
-	}
-
-}
-
-class SSS_QFBunCooker extends QFBunCooker {
-	public SSS_QFBunCooker() {
-		stuffing = "Su Sam Sun";
-	}
-}
-
-class P_GBLBunCooker extends GBLBunCooker {
-	public P_GBLBunCooker() {
-		stuffing = "Pork";
-	}
-}
-
-// ///////////
-// // Simple Factory
-
-class CookGBL {
-	public static Bun make(String bunType, StuffingSource stuffingSource) {
-		Bun bun = null;
-		BunCooker bunCooker = null;
-
-		if (bunType.equals("Pork bun"))
-			bunCooker = new P_GBLBunCooker();
-
-		bunCooker.setStuffingSource(stuffingSource);
-		bun = bunCooker.mix();
-		bun = bunCooker.kneadAndWrap();
-		return bun;
-	}
-}
-
-class CookQF {
-	public static Bun make(String bunType, StuffingSource stuffingSource) {
-		Bun bun;
-		BunCooker bunCooker = null;
-		if (bunType.equals("Green Onion and Pork bun")) {
-			bunCooker = new GOP_QFBunCooker();
-		} else if (bunType.equals("Sam Sun and Pork bun")) {
-			bunCooker = new SSP_QFBunCooker();
-		} else {
-			bunCooker = new SSS_QFBunCooker();
-		}
-
-		bunCooker.setStuffingSource(stuffingSource);
-		bun = bunCooker.mix();
-		bun = bunCooker.kneadAndWrap();
-
-		return bun;
-
-	}
-}
-
-// ///////////
-
-class Waiter {
-	public Bun make(Bun bun) {
-		bun.sb.append("Steamed buns. Dished out buns.");
-		return bun;
+	public String getDesc() {
+		return content.toString();
 	}
 }
--- java/tst/exercise/bunStore/BunStoreTest.java
+++ java/tst/exercise/bunStore/BunStoreTest.java
@@ -3,87 +3,41 @@ package exercise.bunStore;
 import static org.junit.Assert.*;
 
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
-import static org.junit.Assert.assertThat;
-import static org.junit.matchers.JUnitMatchers.*;
-
-import org.junit.experimental.theories.*;
-
-@RunWith(Theories.class)
 public class BunStoreTest {
 
 	private BunStore store;
 
-	@DataPoints
-	public static String[] data() {
-		return new String[] { new String("Monty Burns"),
-				new String("Don Geiss"), new String("Arthur Jensen") };
-	}
-
-	@Theory
-	public void theoryOligarchsHaveYachts(String suit) {
-		assertThat(suit.toString(), containsString("n"));
-	}
-
-	@BeforeClass
-	public static void setUp1() throws Exception {
-		// System.out.println("ba");
-	}
-
 	@Before
 	public void setUp() throws Exception {
 		store = new BunStore();
-		store.setStuffingSource(new ManualStuffing());
 	}
 
 	@Test
 	public void test_QF_Green_Onion_and_Pork_bun() {
-		Bun bun = store.orderQF("Green Onion and Pork bun");
+		Bun bun = store.orderQF("Green Onion and Pork");
 
 		assertEquals(
 				"Mixed stuffing of Green Onion and Pork bun in QF style. Kneaded into dough in QF style. Wrapped buns in QF style. Steamed buns. Dished out buns.",
-				bun.toString());
+				bun.getDesc());
 	}
 
 	@Test
-	public void test_QF_Sam_Sun_and_Pork_bun() {
-		Bun bun = store.orderQF("Sam Sun and Pork bun");
+	public void test_QF_Sam_Sum_and_Pork_bun() {
+		Bun bun = store.orderQF("Sam Sum and Pork");
 
 		assertEquals(
 				"Mixed stuffing of Sam Sum and Pork bun in QF style. Kneaded into dough in QF style. Wrapped buns in QF style. Steamed buns. Dished out buns.",
-				bun.toString());
-	}
-
-	@Test
-	public void test_QF_Su_Sam_Sun_bun() {
-		Bun bun = store.orderQF("Su Sam Sun bun");
-
-		assertEquals(
-				"Mixed stuffing of Su Sam Sun bun in QF style. Kneaded into dough in QF style. Wrapped buns in QF style. Steamed buns. Dished out buns.",
-				bun.toString());
+				bun.getDesc());
 	}
 
 	@Test
 	public void test_GBL_Pork_bun() {
-		Bun bun = store.orderGBL("Pork bun");
+		Bun bun = store.orderGBL("Pork");
 
 		assertEquals(
 				"Mixed stuffing of Pork bun in GBL style. Kneaded into dough in GBL style. Wrapped buns in GBL style. Steamed buns. Dished out buns.",
-				bun.toString());
+				bun.getDesc());
 	}
-
-	@Test
-	public void test_QF_Su_Sam_Sun_bun_From_Ingredient_Factory() {
-		store.setStuffingSource(new FactoryStuffing());
-
-		Bun bun = store.orderQF("Su Sam Sun bun");
-
-		assertEquals(
-				"Got mixed stuffing of Su Sam Sun bun from QF Ingredient Factory. Got flour from QF Ingredient Factory. Prepared stuffing. Kneaded into dough in QF style. Wrapped buns in QF style. Steamed buns. Dished out buns.",
-				bun.toString());
-	}
-
 }

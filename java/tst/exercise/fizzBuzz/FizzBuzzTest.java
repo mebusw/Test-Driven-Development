d706e8368fa69c0c027358718dd3335b7fdcbce2
mebusw <mebusw@163.com>
2013-12-29 15:09:58 +0800

    refactoring to support rule 2.1

--- java/tst/exercise/fizzBuzz/FizzBuzzTest.java
+++ java/tst/exercise/fizzBuzz/FizzBuzzTest.java
@@ -13,17 +13,28 @@ public class FizzBuzzTest {
 
 	@Test
 	public void testOne() {
-		Handler firstHandler = new HandlerForMultiplesOfThree(
-				new HandlerForMultiplesOfFive(
-						new HandlerForOthers(Handler.EMPTY_HANDLER)));
+		// Handler firstHandler = new HandlerForMultiplesOfThree(
+		// new HandlerForMultiplesOfFive(new HandlerForMultiplesOfSeven(new
+		// HandlerForNumberThree(
+		// new HandlerForOthers(Handler.EMPTY_HANDLER)))));
+
+		Handler firstHandler = new HandlerForMultiplesOfThree()
+				.addSuccessor(new HandlerForMultiplesOfFive()
+						.addSuccessor(new HandlerForMultiplesOfSeven()
+								.addSuccessor(new HandlerForNumberThree()
+										.addSuccessor(new HandlerForOthers()))));
 		FizzBuzz fizzBuzz = new FizzBuzz(firstHandler);
-		String results[] = fizzBuzz.play(18);
+		String results[] = fizzBuzz.play(40);
 		assertBySN("1", results, 1);
 		assertBySN("2", results, 2);
 		assertBySN("Fizz", results, 3);
 		assertBySN("4", results, 4);
 		assertBySN("Buzz", results, 5);
+		assertBySN("Whizz", results, 7);
 		assertBySN("FizzBuzz", results, 15);
+		assertBySN("Fizz", results, 23);
+		// assertBySN("FizzBuzzWhizz", results, 35);
+
 	}
 
 	private void assertBySN(String expect, String[] results, int SN) {

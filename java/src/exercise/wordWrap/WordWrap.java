8745283932327ed717b2a2c585bc8fa1e4a29b2d
mebusw <mebusw@163.com>
2014-01-11 14:02:06 +0800

    Word Wrap Kata before TDD Impasse

---
+++ java/src/exercise/wordWrap/WordWrap.java
@@ -0,0 +1,11 @@
+package exercise.wordWrap;
+
+public class WordWrap {
+
+	public static String wrap(String s, int len) {
+		if (s == null)
+			return "";
+		return s;
+	}
+
+}
---
+++ java/tst/exercise/wordWrap/WordWrapTest.java
@@ -0,0 +1,39 @@
+package exercise.wordWrap;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.*;
+
+/**
+ * http://blog.8thlight.com/uncle-bob/2013/05/27/
+ * TheTransformationPriorityPremise.html
+ * 
+ * @author jacky
+ * 
+ */
+public class WordWrapTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	/* ({}–>nil), (nil->constant) */
+	public void WrapNullReturnsEmptyString() throws Exception {
+		assertThat(WordWrap.wrap(null, 10), is(""));
+	}
+
+	@Test
+	/* (constant->constant+) */
+	public void WrapEmptyReturnsEmptyString() throws Exception {
+		assertThat(WordWrap.wrap("", 10), is(""));
+	}
+
+	@Test
+	/* (unconditional->if),(constant->scalar) */
+	public void OneShortWordDoesNotWrap() throws Exception {
+		assertThat(WordWrap.wrap("word", 5), is("word"));
+	}
+}

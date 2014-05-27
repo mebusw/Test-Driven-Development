034540fa68fe51419b370d8e35c13936203206b4
mebusw <mebusw@163.com>
2014-05-27 15:00:48 +0800

    classic Word Wrap kata, with TPP

--- java/src/exercise/wordWrap/WordWrap.java
+++ java/src/exercise/wordWrap/WordWrap.java
@@ -1,16 +1,24 @@
 package exercise.wordWrap;
 
+import sun.security.util.Length;
+
 public class WordWrap {
 
 	public static String wrap(String s, int length) {
 		if (s == null)
 			return "";
-		String result = "";
-		while (s.length() > length) {
-			result += s.substring(0, length) + "\n";
-			s = s.substring(length);
+
+		if (s.length() <= length) {
+			return s;
+		} else {
+			int space = s.substring(0, length + 1).lastIndexOf(" ");
+			if (space >= 0)
+				return s.substring(0, space) + "\n"
+						+ wrap(s.substring(space + 1), length);
+			else
+				return s.substring(0, length) + "\n"
+						+ wrap(s.substring(length), length);
 		}
-		result += s;
-		return result;
 	}
+
 }

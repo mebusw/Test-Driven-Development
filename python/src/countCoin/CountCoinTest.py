b6f3e597ed33302539e7763b0ea8397866c9f93d
JackyShen <mebusw@163.com>
2015-09-30 21:10:13 +0800

    exec: make change with coins

---
+++ python/src/countCoin/CountCoinTest.py
@@ -0,0 +1,52 @@
+"""There are four types of common coins in US currency:
+  quarters (25 cents)
+  dimes (10 cents)
+  nickels (5 cents) 
+  pennies (1 cent)
+  
+There are 6 ways to make change for 15 cents:
+  A dime and a nickel;
+  A dime and 5 pennies;
+  3 nickels;
+  2 nickels and 5 pennies;
+  A nickel and 10 pennies;
+  15 pennies.
+  
+How many ways are there to make change for a dollar
+using these common coins? (1 dollar = 100 cents).
+
+[Source http://rosettacode.org]"""
+
+
+import unittest
+
+def calc(total):
+    QT, TEN, FIVE, ONE = 25, 10, 5, 1
+    result = []
+
+    for q in range(0, total / QT + 1):
+      for t in range(0, (total - QT * q) / TEN + 1):
+        for f in xrange((total - QT * q - TEN * t) / FIVE + 1):
+          result.append([t, f, total - QT * q - TEN * t - FIVE * f])
+
+    return result
+
+class CountCoinTest(unittest.TestCase):
+
+    def test_calc_penny(self):
+        self.assertEquals([[0, 0, 1]], calc(1))
+        self.assertEquals([[0, 0, 4]], calc(4))
+
+    def test_calc_nickel(self):
+        self.assertEquals([[0, 0, 5], [0, 1, 0]], calc(5))
+        self.assertEquals([[0, 0, 6], [0, 1, 1]], calc(6))
+
+    def test_calc_dime(self):
+        self.assertEquals([[0, 0, 10], [0, 1, 5], [0, 2, 0], [1, 0, 0], ], calc(10))
+        self.assertEquals([[0, 0, 11], [0, 1, 6], [0, 2, 1], [1, 0, 1], ], calc(11))
+        self.assertEquals([[0, 0, 15], [0, 1, 10], [0, 2, 5], [0, 3, 0], [1, 0, 5], [1, 1, 0], ], calc(15))
+        print calc(100)
+        print len(calc(100))
+
+if __name__ == '__main__':
+    unittest.main()

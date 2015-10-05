46c62751c1b50473295035090a2046095f6ee911
JackyShen <mebusw@163.com>
2015-10-05 10:40:40 +0800

    exec: make changes with coins, another recursive solution

--- python/src/countCoin/CountCoinTest.py
+++ python/src/countCoin/CountCoinTest.py
@@ -20,8 +20,9 @@ using these common coins? (1 dollar = 100 cents).
 
 import unittest
 
+QT, TEN, FIVE, ONE = 25, 10, 5, 1
+
 def calc(total):
-    QT, TEN, FIVE, ONE = 25, 10, 5, 1
     result = []
 
     for q in range(0, total / QT + 1):
@@ -47,6 +48,39 @@ class CountCoinTest(unittest.TestCase):
         self.assertEquals([[0, 0, 15], [0, 1, 10], [0, 2, 5], [0, 3, 0], [1, 0, 5], [1, 1, 0], ], calc(15))
         print calc(100)
         print len(calc(100))
+
+
+
+def cc(amount, factors, result, a_sln):
+    print result, amount, factors, a_sln
+    factor = factors.pop(0)
+
+    if len(factors) == 0:
+        a_sln = a_sln or []
+        a_sln.extend([amount / factor])
+        result.append(a_sln)
+    else:
+        for i in xrange(amount / factor + 1):
+            a_sln_copy = list(a_sln)
+            a_sln_copy.extend([i])
+            cc(amount - i * factor, list(factors), result, a_sln_copy)
+    return result
+
+class CountCoinRecursiveTest(unittest.TestCase):
+
+    def test_calc_penny(self):
+        self.assertEquals([[4]], cc(4, [ONE], [], []))
+
+    def test_calc_nickel(self):
+        self.assertEquals([[0, 5], [1, 0]], cc(5, [FIVE, ONE], [], []))
+        self.assertEquals([[0, 6], [1, 1]], cc(6, [FIVE, ONE], [], []))
+
+    def test_calc_dime(self):
+        self.assertEquals([[0, 0, 10], [0, 1, 5], [0, 2, 0], [1, 0, 0], ], cc(10, [TEN, FIVE, ONE], [], []))
+        self.assertEquals([[0, 0, 11], [0, 1, 6], [0, 2, 1], [1, 0, 1], ], cc(11, [TEN, FIVE, ONE], [], []))
+        self.assertEquals([[0, 0, 15], [0, 1, 10], [0, 2, 5], [0, 3, 0], [1, 0, 5], [1, 1, 0], ], cc(15, [TEN, FIVE, ONE], [], []))
+        # print calc(100)
+        # print len(calc(100))
 
 if __name__ == '__main__':
     unittest.main()
